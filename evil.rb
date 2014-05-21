ef96a025214b04132ac921194bf386993f7e85a3
Kori Roys <kori@koriroys.com>
2014-05-21 18:05:17 -0500

    minions can be assigned to an evil plan

--- evil.rb
+++ evil.rb
@@ -20,17 +20,30 @@ ActiveRecord::Schema.define do
     t.string :description
     t.integer :overlord_id
   end
+
+  create_table :evil_plot_attendances do |t|
+    t.integer :evil_henchmen_id
+    t.integer :evil_plot_id
+  end
 end
 
 class EvilHenchmen < ActiveRecord::Base
   has_many :minions, class_name: "EvilHenchmen", foreign_key: :overlord_id
   belongs_to :overlord, class_name: "EvilHenchmen"
   has_many :evil_plots, foreign_key: :overlord_id
+  has_many :evil_plot_attendances
+  has_many :evil_plots_being_carried_out, through: :evil_plot_attendances, source: :evil_plot
+end
+
+class EvilPlotAttendance < ActiveRecord::Base
+  belongs_to :evil_henchmen
+  belongs_to :evil_plot
 end
 
 class EvilPlot < ActiveRecord::Base
   belongs_to :overlord, class_name: "EvilHenchmen"
-  # has_many :minions
+  has_many :evil_plot_attendances
+  has_many :minions, through: :evil_plot_attendances, source: :evil_henchmen
 end
 
 sauron = EvilHenchmen.create!(name: "Sauron", weapon: "Mace")
@@ -46,6 +59,8 @@ saruman.minions << uruk_hai
 bind_them_all = EvilPlot.create(codename: "pretty_glitter", description: "Corrupt them all to my will")
 
 sauron.evil_plots << bind_them_all
+
+bind_them_all.minions << saruman
 
 sauron.minions
 saruman.overlord
