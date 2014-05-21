1c46ec1a28476579f52187f21145e4c8496b1a4a
Kori Roys <kori@koriroys.com>
2014-05-21 17:45:15 -0500

    can create evil plots now

--- evil.rb
+++ evil.rb
@@ -14,11 +14,22 @@ ActiveRecord::Schema.define do
     t.string :weapon
     t.integer :overlord_id
   end
+
+  create_table :evil_plots do |t|
+    t.string :codename
+    t.string :description
+    t.integer :evil_henchmen_id
+  end
 end
 
 class EvilHenchmen < ActiveRecord::Base
   has_many :minions, class_name: "EvilHenchmen", foreign_key: :overlord_id
   belongs_to :overlord, class_name: "EvilHenchmen"
+  has_many :evil_plots
+end
+
+class EvilPlot < ActiveRecord::Base
+  belongs_to :evil_henchmen
 end
 
 sauron = EvilHenchmen.create!(name: "Sauron", weapon: "Mace")
@@ -31,7 +42,13 @@ uruk_hai = EvilHenchmen.create!(name: "Uruk-hai", weapon: "Pretty much anything"
 
 saruman.minions << uruk_hai
 
+bind_them_all = EvilPlot.create(codename: "pretty_glitter", description: "Corrupt them all to my will")
+
+sauron.evil_plots << bind_them_all
+
 sauron.minions
 saruman.overlord
+sauron.evil_plots
+bind_them_all.evil_henchmen
 
 binding.pry
